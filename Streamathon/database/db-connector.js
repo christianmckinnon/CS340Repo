363c950f2bde16fb3106fa0d6d15820840d6a100
Christian McKinnon <mckinchr@flip2.engr.oregonstate.edu>
2024-03-15 22:59:51 -0700

    Update database information.

--- Streamathon/database/db-connector.js
+++ Streamathon/database/db-connector.js
@@ -5,9 +5,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_mckinchr',
-    password        : '3753',
-    database        : 'cs340_mckinchr'
+    user            : '*',
+    password        : '*',
+    database        : '*'
 })
 
 // Export it for use in our application
